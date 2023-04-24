5c55e318d654170e12d80ae432f54ab41de41321
Abigail <pineverdi@gmail.com>
2023-04-24 10:00:52 -0600

    boton para agregar varias cajitas

--- src/app/components/mapear/mapear.component.ts
+++ src/app/components/mapear/mapear.component.ts
@@ -14,6 +14,7 @@ declare const pdfjsLib: any; // Agregar esta declaración
 export class MapearComponent implements AfterViewInit {
 
   @ViewChild('container', {static: false}) containerRef!: ElementRef; // Asignar un valor inicial y utilizar {static: false}
+  public mostrarRectangulo = false;
 
   ngAfterViewInit() {
     const container = this.containerRef.nativeElement;
@@ -38,32 +39,64 @@ export class MapearComponent implements AfterViewInit {
         // Crea una nueva capa en el escenario
         const layer = new Konva.Layer();
 
-        // Crea un nuevo rectángulo en la capa
-        const rect = new Konva.Rect({
-          x: 20,
-          y: 20,
-          width: 100,
-          height: 50,
-          fill: 'blue',
-          draggable: true // Habilita la función de arrastrar y soltar en el rectángulo
+        // Verifica si se debe mostrar el rectángulo
+        if (this.mostrarRectangulo) {
+          // Crea un nuevo rectángulo en la capa
+          const rect = new Konva.Rect({
+            x: 20,
+            y: 20,
+            width: 100,
+            height: 50,
+            fill: 'blue',
+            draggable: true // Habilita la función de arrastrar y soltar en el rectángulo
+          });
 
-        });
+          // Añade un evento para escuchar el evento de cambio de posición del rectángulo
+          rect.on('dragend', (event) => {
+            const target = event.target;
+            const x = target.x();
+            const y = target.y();
+            console.log(`Nuevas coordenadas: x=${x}, y=${y}`);
+          });
 
-        // Añade un evento para escuchar el evento de cambio de posición del rectángulo
-        rect.on('dragend', (event) => {
-          const target = event.target;
-          const x = target.x();
-          const y = target.y();
-          console.log(`Nuevas coordenadas: x=${x}, y=${y}`);
-        });
-        
-        // Agrega el rectángulo a la capa
-        layer.add(rect);
+          // Agrega el rectángulo a la capa
+          layer.add(rect);
+        }
 
         // Agrega la capa al escenario
         stage.add(layer);
       });
     });
+  }
+
+  agregarCajita() {
+    const stage = Konva.stages[0]; // Obtén la referencia al escenario de Konva
+    const layer = stage.getLayers()[0]; // Obtén la referencia a la capa del escenario
+
+    // Crea un nuevo rectángulo en la capa
+    const rect = new Konva.Rect({
+      x: 20,
+      y: 20,
+      width: 100,
+      height: 50,
+      fill: 'blue',
+      draggable: true
+    });
+
+    // Añade un evento para escuchar el evento de cambio de posición del rectángulo
+    rect.on('dragend', (event) => {
+      const target = event.target;
+      const x = target.x();
+      const y = target.y();
+      console.log(`Nuevas coordenadas: x=${x}, y=${y}`);
+    });
+
+    // Agrega el rectángulo a la capa
+    layer.add(rect);
+
+    // Redibuja la capa para mostrar el nuevo rectángulo
+    layer.draw();
+  
   }
 
 }
