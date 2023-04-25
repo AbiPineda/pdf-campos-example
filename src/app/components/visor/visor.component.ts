ea9975678986a3407250b97f02c2a6de360af91f
Abigail <pineverdi@gmail.com>
2023-04-25 14:35:26 -0600

    limpiando

--- src/app/components/visor/visor.component.ts
+++ src/app/components/visor/visor.component.ts
@@ -51,35 +51,13 @@ export class VisorComponent implements OnInit {
     rect.on('dragend', (event) => {
       const rectX = rect.x(); // Obtener la posición x del rectángulo después de soltarlo
       const rectY = rect.y(); // Obtener la posición y del rectángulo después de soltarlo
-      console.log('Rectángulo soltado en las coordenadas:', rectX, rectY);
-    });
-    
-    if (this.showRect) {
-    // Agregar evento de soltar al contenedor del PDF
-    const pdfContainer = document.getElementById('pdfContainer');
-    if (pdfContainer) { // Verificar si pdfContainer no es null
-      pdfContainer.addEventListener('drop', (event) => {
-      event.preventDefault();
-      const mouseX = event.clientX; // Obtener la posición del mouse en el eje x
-      const mouseY = event.clientY; // Obtener la posición del mouse en el eje y
-
-      // Obtener la posición relativa del rectángulo dentro del contenedor del PDF
-      const rectX = rect.x() + stage.x();
-      const rectY = rect.y() + stage.y();
-
-      // Verificar si el rectángulo está dentro del área del PDF
-      if (mouseX > rectX && mouseX < rectX + rect.width() && mouseY > rectY && mouseY < rectY + rect.height()) {
-        // El rectángulo está dentro del área del PDF, realizar acciones necesarias
-        console.log('Rectángulo soltado en el PDF');
-      }
+      console.log(`Nuevas coordenadas: x=${rectX}, y=${rectY}`);
     });
 
-    pdfContainer.addEventListener('dragover', (event) => {
-      event.preventDefault();
-    });
-  }
+
+
   }
 }
 
-}
+
 
