de788c67ce3d98eb83fce69b9fecb37a37a79742
Abigail <pineverdi@gmail.com>
2023-04-25 14:11:56 -0600

    limpiando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { DocumentsComponent } from './components/documents/documents.component';
+
 import { VisorComponent } from './components/visor/visor.component';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { DragDropComponent } from './components/drag-drop/drag-drop.component';
 import { MapearComponent } from './components/mapear/mapear.component';
 
 import { VisorPruebaComponent } from './components/visor-prueba/visor-prueba.component';
 
-
 import { HttpClientModule } from '@angular/common/http';
-import { KonvaComponent } from './components/konva/konva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DocumentsComponent,
+
     VisorComponent,
-    DragDropComponent,
+
     MapearComponent,
     VisorPruebaComponent,
-    KonvaComponent
   ],
   imports: [
     BrowserModule,
@@ -34,10 +30,9 @@ import { KonvaComponent } from './components/konva/konva.component';
     FormsModule,
     PdfViewerModule,
     DragDropModule,
-    HttpClientModule
-
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
