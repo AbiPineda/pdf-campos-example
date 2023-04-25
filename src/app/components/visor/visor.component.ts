fbcf07f65f3ecd953c8a320c54040c2fc1feff8d
Abigail <pineverdi@gmail.com>
2023-04-25 14:42:16 -0600

    mostrando rectangulos con sus respectivas coordenadas

--- src/app/components/visor/visor.component.ts
+++ src/app/components/visor/visor.component.ts
@@ -11,42 +11,23 @@ import Konva from 'konva';
 })
 export class VisorComponent implements OnInit {
   pdfUrl: any;
-  showRect = false;
+  rects: Konva.Rect[] = []; // Lista de rectángulos
+  stage!: Konva.Stage; // Referencia al escenario de Konva
 
   constructor(private sanitizer: DomSanitizer) {
-    this.showRect = false;
   }
+
   onButtonPressed(): void {
-    console.log('entre')
-    this.showRect = true; // Cambiar valor de showRect a true
-    this.ngOnInit(); // Llamar a ngOnInit() para actualizar la visibilidad del rectángulo
-    console.log('sali')
-  }
-  ngOnInit(): void {
-    const pdfPath = 'assets/pdf/prueba.pdf';
-    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(pdfPath);
-
-    const stage = new Konva.Stage({
-      container: 'pdfContainer', // ID del contenedor HTML donde se renderizará el lienzo de Konva
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
-
-    const layer = new Konva.Layer();
-    stage.add(layer);
-
     const rect = new Konva.Rect({
       x: 100, // Posición en el eje x
       y: 100, // Posición en el eje y
       width: 100, // Ancho del rectángulo
       height: 100, // Altura del rectángulo
       fill: 'red', // Color de relleno del rectángulo
-      visible: this.showRect
+      visible: true // Establecer visible como true para mostrar el rectángulo
     });
 
     rect.draggable(true); // Habilitar la funcionalidad de arrastrar del rectángulo
-
-    layer.add(rect); // Agregar el rectángulo a la capa
 
     rect.on('dragend', (event) => {
       const rectX = rect.x(); // Obtener la posición x del rectángulo después de soltarlo
@@ -54,10 +35,24 @@ export class VisorComponent implements OnInit {
       console.log(`Nuevas coordenadas: x=${rectX}, y=${rectY}`);
     });
 
+    this.rects.push(rect); // Agregar el rectángulo a la lista de rectángulos
+    const layer = new Konva.Layer();
+    layer.add(rect); // Agregar el rectángulo a la capa de Konva
+    this.stage.add(layer); // Agregar la capa al escenario de Konva
+  }
 
+  ngOnInit(): void {
+    const pdfPath = 'assets/pdf/prueba.pdf';
+    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(pdfPath);
 
+    this.stage = new Konva.Stage({
+      container: 'pdfContainer', // ID del contenedor HTML donde se renderizará el lienzo de Konva
+      width: window.innerWidth,
+      height: window.innerHeight,
+      draggable: true // Habilitar la funcionalidad de arrastrar del lienzo
+    });
+
+    const layer = new Konva.Layer();
+    this.stage.add(layer);
   }
 }
-
-
-
