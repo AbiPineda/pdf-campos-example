577f57730d0ec42c3c1ef41bf14688f1709a2fb7
Abigail <pineverdi@gmail.com>
2023-05-02 13:25:53 -0600

    Revert "mostrar dependiendo de posicion donde se guardo"
    This reverts commit 07e440e8636ecc67a315a99123fe5f7ffe670789.

--- src/app/components/visor/visor.component.ts
+++ src/app/components/visor/visor.component.ts
@@ -3,46 +3,37 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { PDFDocument } from 'pdf-lib';
 import Konva from 'konva';
 
+
 @Component({
   selector: 'app-visor',
   templateUrl: './visor.component.html',
-  styleUrls: ['./visor.component.css'],
+  styleUrls: ['./visor.component.css']
 })
-
 export class VisorComponent implements OnInit {
   pdfUrl: any;
   rects: Konva.Rect[] = []; // Lista de rectángulos
   stage!: Konva.Stage; // Referencia al escenario de Konva
   rectX: number = 0;
   rectY: number = 0;
-  lastRectPosition: { x: number; y: number } = { x: 0, y: 0 }; // Última posición donde se creó un rectángulo
-  parsedData: any;
+  constructor(private sanitizer: DomSanitizer) {
+  }
 
-
-  constructor(private sanitizer: DomSanitizer) {}
-
-  onButtonPressed(buttonIndex: number): void {
-    let x, y;
-    let coordenadas =  this.parsedData[buttonIndex];
-     console.log('algo ', coordenadas)
-
-
+  onButtonPressed(): void {
     const rect = new Konva.Rect({
-      x: coordenadas.x, // Usar la posición x guardada
-      y: coordenadas.y, // Usar la posición y guardada
+      x: 100, // Posición en el eje x
+      y: 100, // Posición en el eje y
       width: 200, // Ancho del rectángulo
       height: 20, // Altura del rectángulo
       fill: '#D2D0D0', // Color de relleno del rectángulo
-      visible: true, // Establecer visible como true para mostrar el rectángulo
+      visible: true // Establecer visible como true para mostrar el rectángulo
     });
 
     rect.draggable(true); // Habilitar la funcionalidad de arrastrar del rectángulo
 
     rect.on('dragend', (event) => {
-      this.lastRectPosition = { x: rect.x(), y: rect.y() }; // Guardar la nueva posición como la última posición
-      localStorage.setItem('lastRectPosition', JSON.stringify(this.lastRectPosition)); // Guardar la última posición en localStorage
-      console.log(`Nuevas coordenadas: x=${this.lastRectPosition.x}, y=${this.lastRectPosition.y}`);
-
+      this.rectX = rect.x(); // Obtener la posición x del rectángulo después de soltarlo
+      this.rectY = rect.y(); // Obtener la posición y del rectángulo después de soltarlo
+      console.log(`Nuevas coordenadas: x=${this.rectX}, y=${this.rectY}`);
     });
 
     this.rects.push(rect); // Agregar el rectángulo a la lista de rectángulos
@@ -50,60 +41,27 @@ export class VisorComponent implements OnInit {
     layer.add(rect); // Agregar el rectángulo a la capa de Konva
 
     //la parte comentareada permite redimencionar el tamaño de los rectangulos
+    
+    // let MAX_WIDTH = 500;
 
-    let MAX_WIDTH = 500;
+    //   let tr = new Konva.Transformer({
+    //     boundBoxFunc: function (oldBoundBox, newBoundBox) {
 
-      let tr = new Konva.Transformer({
-        boundBoxFunc: function (oldBoundBox, newBoundBox) {
+    //       if (Math.abs(newBoundBox.width) > MAX_WIDTH) {
+    //         return oldBoundBox;
+    //       }
 
-          if (Math.abs(newBoundBox.width) > MAX_WIDTH) {
-            return oldBoundBox;
-          }
+    //       return newBoundBox;
+    //     },
+    //   });
+    //   tr.rotateEnabled(false);
 
-          return newBoundBox;
-        },
-      });
-      tr.rotateEnabled(false);
-
-      layer.add(tr);
-      tr.nodes([rect]);
-
+    //   layer.add(tr);
+    //   tr.nodes([rect]);
 
     this.stage.add(layer); // Agregar la capa al escenario de Konva
-
   }
 
-  crearCuadrito(buttonIndex: number): void {
-    let coordenadas =  this.parsedData[buttonIndex];
-    if(coordenadas === null) {
-      coordenadas = {x:20, y:20};
-    }
-
-    const rect = new Konva.Rect({
-      x: coordenadas.x, // Usar la posición x guardada
-      y: coordenadas.y, // Usar la posición y guardada
-      width: 200, // Ancho del rectángulo
-      height: 20, // Altura del rectángulo
-      fill: '#D2D0D0', // Color de relleno del rectángulo
-      visible: true, // Establecer visible como true para mostrar el rectángulo
-    });
-
-    rect.draggable(true); // Habilitar la funcionalidad de arrastrar del rectángulo
-
-    rect.on('dragend', (event) => {
-      // this.lastRectPosition = { x: rect.x(), y: rect.y() }; // Guardar la nueva posición como la última posición
-      this.parsedData[buttonIndex] = {x: rect.x(), y: rect.y()};
-      let jsonData = JSON.stringify(this.parsedData);
-      localStorage.setItem("data",jsonData);
-      console.log(`Nuevas coordenadas: x=${this.lastRectPosition.x}, y=${this.lastRectPosition.y}`);
-
-    });
-
-    this.rects.push(rect); // Agregar el rectángulo a la lista de rectángulos
-    const layer = new Konva.Layer();
-    layer.add(rect);
-    this.stage.add(layer);
-  }
   ngOnInit(): void {
     const pdfPath = 'assets/pdf/prueba.pdf';
     this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(pdfPath);
@@ -112,26 +70,10 @@ export class VisorComponent implements OnInit {
       container: 'pdfContainer', // ID del contenedor HTML donde se renderizará el lienzo de Konva
       width: window.innerWidth,
       height: window.innerHeight,
-      draggable: true, // Habilitar la funcionalidad de arrastrar del lienzo
+      draggable: true // Habilitar la funcionalidad de arrastrar del lienzo
     });
 
     const layer = new Konva.Layer();
     this.stage.add(layer);
-
-    const storedData = localStorage.getItem("data");
-
-    if (storedData) {
-      this.parsedData = JSON.parse(storedData);
-      console.log(this.parsedData);
-    }
-
-    this.cargarPDF(pdfPath);
-  }
-
-  async cargarPDF(pdfPath: string) {
-    const pdfBytes = await fetch(pdfPath).then((res) => res.arrayBuffer());
-    const pdfDoc = await PDFDocument.load(pdfBytes);
-    const pageCount = pdfDoc.getPageCount();
-    console.log(`El archivo PDF tiene ${pageCount} páginas.`);
   }
 }
